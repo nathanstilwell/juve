58552ba2214765db29a37e9591c91c826fc8bdb6
Jared Stilwell <jared.k.stilwell@gmail.com>
2014-01-20 08:37:14 -0600

    Fixes #6

--- test/juve_test.js
+++ test/juve_test.js
@@ -20,3 +20,22 @@ exports.basic_usage = function (test) {
 	});
 };
 
+exports.cached_trials = function (test) {
+	test.expect(3);
+
+	juve({
+
+		trials: 2,
+		url: 'http://localhost:9002/blank.html',
+		assertions: { requests: 1 }
+
+	}, function (passes, failures, trials) {
+
+		test.equal( trials.length, 2 );
+
+		test.equal( trials[0].metrics.requests, 1 );
+		test.equal( trials[1].metrics.requests, 1 );
+		test.done();
+
+	});
+};
